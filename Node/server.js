52fddbd75ecc312ed7071eeaa76f295da400f154
PniniKlein <pnini4662@gmail.com>
2025-02-06 23:46:34 +0200

    node 2

--- Node/server.js
+++ Node/server.js
@@ -1,29 +1,19 @@
-const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
+import express from 'express';
+import renderApi from '@api/render-api';
 
 const app = express();
-//const PORT = 3000;
-const PORT = process.env.PORT || 3000; // שימוש בפורט מהסביבה אם קיים
+const PORT = process.env.PORT || 3000;
 
+// אימות מול Render API
+renderApi.auth('rnd_vXCKd8e8g8cCOnFhOUAqvOyVUPbm');
 
 app.get('/', async (req, res) => {
   try {
-    // קבלת ה-API Key מהסביבה
-    const apiKey = process.env.RENDER_API_KEY;
-
-    // שליחת בקשה ל-Render API כדי לקבל את רשימת האפליקציות
-    const response = await axios.get('https://api.render.com/v1/services', {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
-
-    // החזרת נתוני האפליקציות ב-JSON
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching data from Render API:', error);
-    res.status(500).json({ message: 'Error fetching data' });
+    const { data } = await renderApi.listServices({ includePreviews: 'true', limit: '20' });
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching data from Render API');
   }
 });
 
